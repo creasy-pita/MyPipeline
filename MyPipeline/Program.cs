e232404b68cd2436e9b7de5d0ddb1debb775d380
creasy-pita <hz_ljq@live.com>
2018-02-10 12:34:17 +0800

    Initial commit

---
+++ MyPipeline/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyPipeline
+{
+    class Program
+    {
+        public static List<Func<RequestDelegate, RequestDelegate>> _list = new List<Func<RequestDelegate, RequestDelegate>>();
+
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            Use((next) =>
+            {
+                return context =>
+                {
+                    Console.WriteLine("11  doing my  start logic");
+                    Task ins = next.Invoke(context);
+                    Console.WriteLine("11 doing my  stop logic");
+                    return ins;
+                };
+            });
+            Use((next) =>
+            {
+                return context =>
+                {
+                    Console.WriteLine("22  doing my  start logic");
+                    Task ins= next.Invoke(context);
+                    Console.WriteLine("22 doing my  stop logic");
+                    return ins;
+                };
+            });
+            RequestDelegate end = (context =>
+                {
+                    Console.WriteLine("end...");
+                    return Task.CompletedTask;
+                }
+            );
+            _list.Reverse();
+            foreach(var middleware in _list)
+            {
+                //Func<RequestDelegate , RequestDelegate>  输入 RequestDelegate 输出RequestDelegate
+                //通过Reverse  然后从尾部传入 end , middleware.Invoke(end) 传入了 每个middleware的RequestDelegate next参数
+                //使得整条链接形成通路
+                end = middleware.Invoke(end);
+            }
+            end.Invoke(new Context());
+            Console.ReadLine();
+
+        }
+
+        //传入Func<RequestDelegate , RequestDelegate>类型的 middleware， 加入_list 但还没有调用，待使用
+        public static  void Use(Func<RequestDelegate , RequestDelegate> middleware)
+        {
+            _list.Add(middleware);
+        }
+
+
+
+    }
+}
