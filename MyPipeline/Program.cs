5657acfd7929cbbcd40d027e23e77a0489ff29b3
creasy-pita <hz_ljq@live.com>
2018-02-10 14:13:15 +0800

    加入注释

--- MyPipeline/Program.cs
+++ MyPipeline/Program.cs
@@ -11,11 +11,15 @@ namespace MyPipeline
         static void Main(string[] args)
         {
             // Console.WriteLine("Hello World!");
-
+            DoProccess1();
+        }
+        static void DoProccess2()
+        {
             Use((next) =>
             {
                 return context =>
                 {
+                    //处理 start logic1, 进入next处理， next.invoke结束后处理 stop logic2
                     Console.WriteLine("11  doing my  start logic");
                     Task ins = next.Invoke(context);
                     Console.WriteLine("11 doing my  stop logic");
@@ -27,28 +31,62 @@ namespace MyPipeline
                 return context =>
                 {
                     Console.WriteLine("22  doing my  start logic");
-                    Task ins= next.Invoke(context);
+                    Task ins = next.Invoke(context);
                     Console.WriteLine("22 doing my  stop logic");
                     return ins;
                 };
             });
             RequestDelegate end = (context =>
-                {
-                    Console.WriteLine("end...");
-                    return Task.CompletedTask;
-                }
+            {
+                Console.WriteLine("end...");
+                return Task.CompletedTask;
+            }
             );
             _list.Reverse();
-            foreach(var middleware in _list)
+            foreach (var middleware in _list)
             {
                 //Func<RequestDelegate , RequestDelegate>  输入 RequestDelegate 输出RequestDelegate
                 //通过Reverse  然后从尾部传入 end , middleware.Invoke(end) 传入了 每个middleware的RequestDelegate next参数
                 //使得整条链接形成通路
                 end = middleware.Invoke(end);
             }
+            //调用的时候 end 中的 next 已经在之前 _list 遍历中传入 
             end.Invoke(new Context());
             Console.ReadLine();
+        }
 
+        static void DoProccess1()
+        {
+
+            Use((next) => {
+                return (context) =>
+                {
+                    Console.WriteLine("tran begin");
+                    Task t = next.Invoke(context);
+                    Console.WriteLine("tran end");
+                    return t;
+                };
+            });
+
+            Use((next) => {
+                {
+                    Console.WriteLine("log begin");
+                    Task t = next.Invoke(context);
+                    return t;
+                };
+            });
+            RequestDelegate end = (context) => {
+                Console.WriteLine("ending ....");
+                return Task.CompletedTask;
+            };
+
+            _list.Reverse();
+            foreach (var middleware in _list)
+            {
+                end = middleware.Invoke(end);
+            }
+            end.Invoke(new Context());
+            Console.ReadLine();
         }
 
         //传入Func<RequestDelegate , RequestDelegate>类型的 middleware， 加入_list 但还没有调用，待使用
